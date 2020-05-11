d1aaf49e414a82dd42dc1a023d91eb74acba5718
Damian Sztuczka <gardanw@gmail.com>
2020-05-11 15:04:25 +0200

    funkcaj dodajaca tla lokacji

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def load_img(path):
     pass
 
 
+def create_screen(location, path_to_image, game):
+    screen = Screen(location, game)
+    screen.add_element(ScreenElement(0, 0, path_to_image, game))
+    return screen
+
+
 def main():
     game = Game("arrow", 'assets/guiicons/Assets/GUI_Icons/GUI_Icons_png/transparent/arrow_s_t.png', width=1280,
                 height=720, color=(0, 0, 0))
@@ -24,106 +30,10 @@ def main():
     game.add_element(player)
 
     # background
-    screen = Screen([0, -1], game)
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 0,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_111.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_127.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 128,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_143.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 192,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_111.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 256,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_127.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 5,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_143.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 6,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_111.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 7,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_127.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 8,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_143.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 9,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_111.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 10,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_127.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 11,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_143.png',
-                                         game))
-    game.add_background(screen)
-    screen = Screen([0, 1], game)
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 0,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_154.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_170.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 128,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_186.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 192,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_154.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 256,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_170.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 5,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_186.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 6,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_154.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 7,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_170.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 8,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_186.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 9,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_154.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 10,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_170.png',
-                                         game))
-    for i in range(20):
-        screen.add_element(ScreenElement(i * 64, 64 * 11,
-                                         r'assets\2dhandpainted_towntileset\Assets\64x64\Town@64x64_crop\Town@64x64_186.png',
-                                         game))
-    game.add_background(screen)
+    s1 = create_screen([0, 0], r'assets\2dhandpainted_towntileset\Assets\64x64\grass.png', game)
+    s2 = create_screen([0, 0], r'assets\2dhandpainted_towntileset\Assets\64x64\bar.png', game)
+    game.add_background(s1)
+    game.add_background(s2)
     game.run()
 
 
